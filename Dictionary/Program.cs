ff2c1093254ad96c2a6fcbb944948da8c2baafa3
farhullin2017@gmail.com <90451035+IlnazFarkhullin@users.noreply.github.com>
2024-01-20 10:46:46 +0300

    словарь

---
+++ Dictionary/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                //Объявляем словарь
+                Dictionary<int, string> collection = new Dictionary<int, string>()
+               {
+                //заполняем словарь данными
+                {1210, "Кабинет Ильназа Алмазовича" },
+                {302, "Кабинет Талгата Рамилевича" },
+                {1305, "Кабинет Лилии Ринатовны"},
+              
+
+                //также можно заполнить и по другому
+                //[1210] =  "Кабинет ОАиП",
+                //[302] = "Кабинет выш.мат",
+                //[1311] = "Кабинет зав.отделением"
+                };
+                collection.Add(1215, "Кабинет Ларисы Гавриловны");//Добавляем новый элемент в словарь
+                foreach (var item in collection)
+                {
+                    Console.WriteLine($"ID {item.Key} = {item.Value}");// выводим на консоль словарь с добавлением нового элемента
+                }
+                Console.WriteLine();
+
+                collection.Remove(302); //удаляем элемент с ID 302
+                foreach (var item in collection)
+                {
+                    Console.WriteLine($"ID {item.Key} = {item.Value}"); //выводим на консоль словарь с удаленным элементом
+
+                }
+                Console.WriteLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($" В словаре обнаружено два одинаковых ID ключа");// исключение на слущай обнаружения двух одинаковых ID
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Dictionary_2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                    //Заполнить очередь именами студентов из группы. 
+                    //Создать словарь где ID записи будет генерироваться с помощью класса Random
+                    //а значения будут браться из созданной ранее очереди
+                    //Затем пользователь вводит случайное число. Определить  есть ли запись с ID равное числу который ввёл пользователь
+            try
+            {
+
+                    Queue students = new Queue();
+                    students.Enqueue("Белов Юрий");
+                    students.Enqueue("Сафина Ильдина");
+                    students.Enqueue("Платонова Полина");
+                    students.Enqueue("Рюхова Софья");
+                    students.Enqueue("Кулыев Денис");
+                    students.Enqueue("Строй Владислав");
+                    students.Enqueue("Данилова Даша");
+                    students.Enqueue("Кузнецова Лера");
+
+                    Random random = new Random();
+                    Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+                    foreach (var stud in students)
+                    {
+                        int key = random.Next(0,100); 
+                        dictionary.Add(key, stud.ToString());
+                    }
+
+                    foreach (var d in dictionary)
+                    {
+                        Console.WriteLine($"ID: {d.Key} - ФИО: {d.Value}");
+                    }
+
+                Console.WriteLine("Введите ID:");
+                int id = Convert.ToInt32(Console.ReadLine());
+                if (dictionary.ContainsKey(id))
+                {
+                    Console.WriteLine($"Запись с ID {id} существует. Имя студента: {dictionary[id]}");
+                }
+                else
+                {
+                    Console.WriteLine($"Запись с ID {id} не найдена");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
